1ad9cba0854265159da8fba3f1430dced54ce92c
Your Name <you@example.com>
2024-03-30 18:42:22 +0500

    hero done

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing", {
+    strings: [" Custom Web Developer", "Wordpress Developer", "Wix Web Designer" , "UI/UX Designer", "Freelancer", "Video Editor"],
+    typeSpeed: 60,
+    backSpeed: 30,
+    loop: true
+});
